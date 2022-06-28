0157e71c74b1534d252afe549dc423ecb7175beb
tuankiet0901 <kiet-lt@genkisystem.com>
2022-06-28 17:55:07 +0700

    First Commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,35 @@
 const { defineConfig } = require("@vue/cli-service");
+const { ModuleFederationPlugin } = require("webpack").container;
+const deps = require("./package.json").dependencies;
 module.exports = defineConfig({
+  publicPath: "auto",
+  configureWebpack: {
+    optimization: {
+      splitChunks: false,
+    },
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "running",
+        filename: "remoteEntry.js",
+        remotes: {
+          downed: "downed@http://localhost:8091/remoteEntry.js",
+        },
+        exposes: {},
+        shared: {
+          ...deps,
+          vue: {
+            eager: true,
+            singleton: true,
+            requiredVersion: deps.vue,
+            strictVersion: true,
+          },
+        },
+      }),
+    ],
+  },
+  devServer: {
+    port: 8090,
+    historyApiFallback: true,
+  },
   transpileDependencies: true,
 });
